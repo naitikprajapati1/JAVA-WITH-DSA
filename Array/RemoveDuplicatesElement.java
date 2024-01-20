9e47ee341936f6cd3b951831790f4383f88420a1
naitikprajapati1 <naitikprajapti.dev@gmail.com>
2024-01-20 22:54:23 +0530

    chnages on array

---
+++ Array/RemoveDuplicatesElement.java
@@ -0,0 +1,8 @@
+package Array;
+
+public class RemoveDuplicatesElement {
+    public static void main(String[] args) {
+        int[] arr = { 1, 1, 2, 2, 3, 3, 3, 4, 5, 5, 6, 6, 7, 7, 8, 8 };
+
+    }
+}
---
+++ Array/ReverseArray.java
@@ -0,0 +1,10 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] arr = { 6, 4, 1, 12, 3, 1 };
+        int[] temp = new int[arr.length];
+        // for (int i = 0; i < temp.length; i++) {
+        // System.out.println(temp[i]);
+        // }
+    }
+
+}
